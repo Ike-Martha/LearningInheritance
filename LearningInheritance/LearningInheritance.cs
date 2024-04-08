1af3e14dee47ee553b7771db8726a00f5713d964
Ike-Martha <amagwen36@gmail.com>
2024-04-08 15:10:58 +0100

    Add project files.

---
+++ LearningInheritance/LearningInheritance.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningInheritance
+{
+    internal abstract class Vehicle
+    {
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+
+        public abstract string FormatMe();
+    }
+
+    class Car : Vehicle
+    {
+        public override string FormatMe()//helper method//the virtual keyword permits you to 
+                                         //take from this method while the abstract also permits but you must make some changes
+        {
+            return string.Format("{0} - {1} - {2} - {3}",
+                this.Make,
+                this.Model,
+                this.Year,
+                this.Color);
+        }
+
+    }
+
+
+
+        sealed class Truck : Vehicle//: represents inheritance
+                                    //sealed keyword means nobody can ever inherit fro this class
+        {
+            public int TowingCapacity { get; set; }
+            public override string FormatMe()
+            {
+                return string.Format("{0} - {1} - {2} Towing unit",
+                this.Make,
+                this.Model,
+                this.TowingCapacity);
+
+            }
+        }
+
+        class Bike : Vehicle
+        {
+            public int SafetyMeasure { get; set; }
+            public override string FormatMe()
+            {
+                return string.Format("{0} - {1} - {2} Headcaps for rider and one passenger",
+                    this.Make,
+                    this.Model,
+                    this.Year,
+                    this.SafetyMeasure);
+            }
+
+
+
+        }
+
+        class Bycicle : Vehicle
+        {
+            public string Route { get; set; }
+            public override string FormatMe()
+            {
+                return string.Format("{0} - {1} -{2} make use of the service lane",
+                    this.Make,
+                    this.Model,
+                    this.Year,
+                    this.Route);
+            }
+
+        }
+}
+
+
+
+
---
+++ LearningInheritance/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using LearningInheritance;
+
+namespace LearningInheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car();
+            myCar.Make = "BMW";
+            myCar.Model = "333li";
+            myCar.Year = 2001;
+            myCar.Color = "gray";
+            printVehicleDetails(myCar);
+
+            Truck myTruck = new Truck();
+            myTruck.Make = "BMW";
+            myTruck.Model = "999JK";
+            myTruck.Year = 2005;
+            myTruck.Color = "red";
+            myTruck.TowingCapacity = 200;
+            printVehicleDetails(myTruck);
+
+            Bike myBike = new Bike();
+            myBike.Make = "BNW";
+            myBike.Model = "lk9";
+            myBike.Year = 2010;
+            myBike.Color = "Blue";
+            myBike.Year = 2019;
+            myBike.SafetyMeasure = 2;
+            printVehicleDetails(myBike);
+
+
+            Bycicle myBycicle = new Bycicle();
+            myBycicle.Make = "BMW";
+            myBycicle.Model = "xxx";
+            myBycicle.Year = 2020;
+            myBycicle.Color = "Green";
+            myBycicle.Route = "Service Lane";
+            printVehicleDetails(myBycicle);
+            Console.ReadLine();
+
+        }
+
+
+
+        private static void printVehicleDetails(Vehicle vehicle)//helper method for program class
+        {
+
+            Console.WriteLine("Here are the Vehicle's details: {0}", vehicle.FormatMe());
+        }
+
+
+    }
+}
